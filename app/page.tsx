8406b1e13497337bee714d3a6204a3ba4700581c
Rio Rifaldi <riorifaldi@MacBook-Air.local>
2024-06-01 11:09:43 +0700

    initial commit : TodoApp with NextJS + Shadcn UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,107 @@
-import Image from "next/image";
+"use client";
+
+import {Button} from "@/components/ui/button";
+import {Dialog, DialogContent, DialogTitle, DialogTrigger} from "@/components/ui/dialog";
+import {Input} from "@/components/ui/input";
+import {FormEvent, useEffect, useState} from "react";
+
+type InputTodo = {
+	id: string;
+	value: string;
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [todos, setTodo] = useState<InputTodo[]>([]);
+	const [input, setInput] = useState("");
+	const [openDialog, setOpenDialog] = useState(false);
+	const [updateTodo, setUpdateTodo] = useState("");
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const onsubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const newInput = {id: new Date().getTime().toString(), value: input};
+		setTodo((prev) => [...prev, newInput]);
+		localStorage.setItem("todos", JSON.stringify([...todos, newInput]));
+		setInput("");
+	};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const handleClear = () => {
+		localStorage.clear();
+		setTodo([]);
+	};
+	const handdleDelete = (id: string) => {
+		const newTodo = todos.filter((todo) => todo.id !== id);
+		localStorage.setItem("todos", JSON.stringify(newTodo));
+		setTodo(newTodo);
+	};
+	const handdleUpdate = (e: FormEvent<HTMLFormElement>, id: string) => {
+		e.preventDefault();
+		const filterTodo = todos.filter((todo) => todo.id !== id);
+		if (updateTodo.length !== 0) {
+			const newTodo = [...filterTodo, {id: new Date().getTime().toString(), value: updateTodo}];
+			setTodo(newTodo);
+			localStorage.setItem("todos", JSON.stringify(newTodo));
+			setUpdateTodo("");
+		}
+	};
+	useEffect(() => {
+		const local = localStorage.getItem("todos");
+		if (local) {
+			const storedTodo = JSON.parse(local);
+			setTodo(storedTodo);
+		}
+	}, []);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	return (
+		<main className="mx-6 my-8 max-w-screen-md sm:mx-auto">
+			<form action="" onSubmit={onsubmit} autoComplete="off">
+				<div className="flex gap-2 items-end">
+					<div className="grid gap-3 w-full">
+						<input type="text" name="text" id="text" placeholder="masukan sini" className="px-5 py-3 rounded-md text-black" onChange={(e) => setInput(e.target.value)} value={input} required />
+					</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+					<button type="submit" className=" h-max font-medium px-6 py-3 rounded bg-blue-500 hover:bg-blue-700 transition-colors  text-white">
+						kirim
+					</button>
+				</div>
+			</form>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+			<ul className="my-6 grid ">
+				{todos.length !== 0 &&
+					todos.map((todo, index) => (
+						<div key={todo.id}>
+							<Dialog>
+								<div className=" flex items-center gap-3 ">
+									<li className="bg-purple-800 rounded-md py-2 px-4 w-full text-white">{todo.value}</li>
+									<div className="flex gap-1 items-center">
+										<button className=" text-white py-2 px-3 rounded-md my-3 bg-red-500 hover:bg-red-700" onClick={() => handdleDelete(todo.id)}>
+											Delete
+										</button>
+
+										<DialogTrigger asChild>
+											<Button size={"sm"} variant={"outline"}>
+												{" "}
+												Update{" "}
+											</Button>
+										</DialogTrigger>
+									</div>
+								</div>
+								<DialogContent>
+									<DialogTitle className=""> update Todo '{todo.value}'</DialogTitle>
+									<form action="" onSubmit={(e) => handdleUpdate(e, todo.id)}>
+										<Input className="my-5" onChange={(e) => setUpdateTodo(e.target.value)} value={updateTodo} placeholder={`previous todo : ${todo.value}`} />
+
+										<Button type="submit"> Update </Button>
+									</form>
+								</DialogContent>
+							</Dialog>
+						</div>
+					))}
+			</ul>
+			{todos.length !== 0 && (
+				<button className="text-white py-2 px-4 rounded-md my-3 bg-orange-500 hover:bg-orange-700" onClick={handleClear}>
+					clear
+				</button>
+			)}
+		</main>
+	);
 }
